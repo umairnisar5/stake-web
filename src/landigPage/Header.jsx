b54259678badc1766f8fb361b51d93f119815634
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-01-07 02:35:56 +0500

    updated html decentralized

--- src/landigPage/Header.jsx
+++ src/landigPage/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import logo from "./zyflogonew.png"
+import logo from "./zyflogonew.png";
+import image from "./pic1.png"
 import "./LandingPage.css"
 function Header() {
     return (
+        <div>
         <div className="container">
             <div className="logo-div">
   <span className="logo-img">
@@ -12,20 +14,29 @@ function Header() {
 zYF Protocol
   </span>
             </div>
-            <div className="text_blog">
-                 <span>
+            <div>
+                 <span className="text_blog">
                    Blog
                  </span>
                  <span>
                    Launch App 
                  </span>
             </div>
-
-            <div>
-            
-            </div>
+        </div>
+        <div className="decentr_lized">
+        <h1>Decentralized<br/> Seigniorage Insurance<br/> Protocol</h1>
+        <img src={image} alt="pic" width="100px" />
 
         </div>
+        
+
+
+
+</div>
+        
+       
+        
+        
        
 
     )
