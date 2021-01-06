1f4bc3b7c292bda3b709e1e49c3e329e1a6006a2
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-01-07 00:58:52 +0500

    Creating new components

---
+++ src/landigPage/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div>
+             <div>
+             <span>
+    <img src="" alt="" />
+             </span>
+             <span>
+  <h1>zYF Protocol</h1>
+             </span>
+             </div>
+             <div>
+
+             </div>
+        </div>
+    )
+}
