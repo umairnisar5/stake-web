46ae882b717ff9adefa08a190f622639872e373a
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-01-07 04:10:56 +0500

    updated html and icons

--- src/landigPage/Header.jsx
+++ src/landigPage/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import logo from "./zyflogonew.png";
-import image from "./pic1.png"
+import image from "./pic1.png";
+import pic from "./Untitled.png";
+import NearMeIcon from '@material-ui/icons/NearMe';
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 import "./LandingPage.css"
 
 function Header() {
@@ -22,6 +25,9 @@ zYF Protocol
                  <span>
                    Launch App 
                  </span>
+                 <div className="arrow_icon">
+                 <ArrowRightAltIcon />
+                 </div>
             </div>
         </div>
         <div className="decentr_lized">
@@ -31,7 +37,7 @@ zYF Protocol
         </div>
         <div className="para_div">
             <p>
-                Protect your Seigniorage assets the risk of not returning to peg
+                Protect your Seigniorage assets the risk of not returning to peg<br/>
                 (with a specific time period or ever). Get paid providing liquidity to the zYF insurance fund.
 
             </p>
@@ -47,15 +53,33 @@ zYF Protocol
         </p>
         </div>
 
-        <div>fonts</div>
+        <div>
+       <NearMeIcon />
+       </div>
 
 
+        <div main_div>
         <div className="protocol">
         <h2>What is zYF Protocol?</h2>
         <h3>Insurance on coupons</h3>
-        <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod</p>
-
+        <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor<br/>
+        incididunt ut labore et dolore manage alique. Ut enim ad minim veniam,quis nostrud<br/>
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
+        <div className="pegging">
+            <h2>Insurance on un-pegging</h2>
+            <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor<br/>
+        incididunt ut labore et dolore manage alique. Ut enim ad minim veniam,quis nostrud
+        exercitation</p>
+        </div>
+        <img src={pic} alt="img" width="100px" />
+
+        <button className="read_blog">Read our blog</button>
+        </div>
+        <footer>
+           <p> zYF Protocol 2021</p>
+           <p>App Lite paper Blog</p>
+        </footer>
 
         
 
