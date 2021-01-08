b60851f3fbc3697104f82474d689c51ae5ba5931
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-01-08 18:10:10 +0500

    update landing css

--- src/landigPage/Header.jsx
+++ src/landigPage/Header.jsx
@@ -11,75 +11,93 @@ import "./LandingPage.css";
 function Header() {
   return (
     <div>
-      <div className="container">
-        <div className="logo-div">
-          <span className="logo-img">
-            <img src={logo} alt="logo" width="60px" />
-          </span>
-          <span className="logo-txt">zYF Protocol </span>
-        </div>
-        <div className="text_blog">
-          <span className="blog_text">Blog </span>
-          <span className="launch_app">LaunchApp </span>
-          <ArrowRightAltIcon className="arrow_icon" />
-        </div>
-      </div>
-      <div>
-        <div className="decentr_lized">
-          <div className="banertxt-div">
-            <h1>
-              Decentralized <br /> Seigniorage Insurance <br /> Protocol
-            </h1>
-            <p className="para_div">
-              Protect your Seigniorage assets the risk of not returning to
-              peg(with a specific time period or ever).Get paid providing
-              liquidity to the zYF insurance fund.
-            </p>
+      <div className="blue-color">
+        <div className="container">
+          <div className="logo-div">
+            <span className="logo-img">
+              <img src={logo} alt="logo" width="60px" />
+            </span>
+            <span className="logo-txt">zYF Protocol</span>
           </div>
+          <div className="text_blog">
+            <span className="blog_text">Blog</span>
+            <span className="launch_app">LaunchApp</span>
+            <ArrowRightAltIcon className="arrow_icon" />
+          </div>
+        </div>
+        <div>
+          <div className="decentr_lized">
+            <div className="banertxt-div">
+              <h1>
+                Decentralized
+                <br /> Seigniorage Insurance
+                <br /> Protocol
+              </h1>
 
-          <img className="image" src={image} alt="pic" />
-        </div>
-        <div className="button_launch">
-          <button className="launch_text"> Launch zzApp </button>
-          <button className="lite_paper"> Litepaper </button>
-        </div>
-        <div className="join">
-          <p>join our community!</p>
+              <p className="para_div">
+                Protect your Seigniorage assets the risk of not returning to peg
+                (with a specific time period or ever). Get paid providing
+                liquidity to the zYF insurance fund.
+              </p>
+            </div>
+
+            <img className="image" src={image} alt="pic" />
+          </div>
+          <div className="button_launch">
+            <button className="launch_text">Launch App</button>
+            <button className="lite_paper">Litepaper</button>
+          </div>
+          <div className="join">
+            <p className="community">join our community!</p>
+            <div className="logo-div">
+              <ModeCommentIcon />
+              <NearMeIcon />
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <ModeCommentIcon />
-        <NearMeIcon />
-      </div>
-      <div main_div>
+
+      <div className="main_div">
         <div className="protocol">
-          <h2> What is zYF Protocol ? </h2> <h3> Insurance on coupons </h3>
+          <h2 className="what">What is zYF Protocol?</h2>
+          <h3>Insurance on coupons</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor <br />
-            incididunt ut labore et dolore manage alique.Ut enim ad minim
-            veniam, quis nostrud <br />
+            Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do
+            eiusmod tempor
+            <br />
+            incididunt ut labore et dolore manage alique. Ut enim ad minim
+            veniam,quis nostrud
+            <br />
             exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat.
           </p>
         </div>
         <div className="pegging">
-          <h2> Insurance on un - pegging </h2>
+          <h2 className="insurr">Insurance on un-pegging</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor <br />
-            incididunt ut labore et dolore manage alique.Ut enim ad minim
-            veniam, quis nostrud exercitation
+            Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do
+            eiusmod tempor
+            <br />
+            incididunt ut labore et dolore manage alique. Ut enim ad minim
+            veniam,quis nostrud exercitation
           </p>
         </div>
-        <img src={pic} alt="img" width="100px" />
-        <button className="read_blog"> Read our blog </button>
+        <div className="second_img">
+          <img src={pic} alt="img" width="450px" />
+        </div>
+
+        <button className="read_blog">Read our blog</button>
       </div>
-      <footer className="foot_div">
-        <p> zYF Protocol© 2021 </p> <p> App Lite paper Blog </p> <TwitterIcon />
-        <ModeCommentIcon />
-        <NearMeIcon />
-      </footer>
+      <div className="footer_div">
+        <p className="zyf"> zYF Protocol © 2021</p>
+        <h6>App Lite paper Blog</h6>
+
+        <div className="tw_near">
+          <TwitterIcon />
+          <ModeCommentIcon />
+          <NearMeIcon />
+        </div>
+      </div>
     </div>
   );
 }
